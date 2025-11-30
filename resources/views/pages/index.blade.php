152fc2a165802a25ce1a5fa51a2d4528038dea87
vicsystems <victorasuquob@gmail.com>
2025-11-30 21:08:01 +0100

    site update

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -36,7 +36,7 @@
                 </div>
                 <div class="swiper-slide">
                     <div class="hero-inner" data-mask-src="assets/img/hero/hero_1_bg_mask.png">
-                        <div class="th-hero-bg" data-bg-src="assets/img/hero/hero_bg_1_2.jpg"></div>
+                        <div class="th-hero-bg" data-bg-src="assets/img/hero/hero_bg_1_55.jpg"></div>
                         <div class="hero-big-text">QUALITY</div>
                         <div class="container">
                             <div class="row align-items-center">
